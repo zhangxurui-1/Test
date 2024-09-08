137414ee3e2eb39bd3529361c9e29c6a96eeacdb
zhangxurui-1 <18862765404@163.com>
2024-09-09 00:33:27 +0800

    update

--- closure.go
+++ closure.go
@@ -1,19 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func foo(x int) []func() {
 	var fs []func()
 	var values = []int{1, 2, 3, 4, 5}
-	for _, v := range values {
+	for _, val := range values {
 		fs = append(fs, func() {
-			fmt.Printf("%v\n", v+x)
+			fmt.Printf("foo value: \t%v\n", val+x)
 		})
 	}
 
 	return fs
 }
 
+func Output() {
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Printf("%d ", i)
+		}()
+		// time.Sleep(1 * time.Second)
+	}
+	fmt.Printf("\nOutput() end\n\n")
+}
+
+func Output2() {
+	for i := 0; i < 10; i++ {
+		i := i
+		go func() {
+			fmt.Printf("%d ", i)
+		}()
+	}
+	fmt.Printf("\nOutput2() end\n\n")
+}
+
 func ClosureTest() {
-	foo(10)
+	var fs = foo(10)
+	for _, f := range fs {
+		f()
+	}
+
+	Output()
+	Output2()
 }

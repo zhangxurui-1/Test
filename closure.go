0df94dfc972b6574ad58eec21f1876a8d80f5554
zhangxurui-1 <18862765404@163.com>
2024-09-09 14:28:15 +0800

    update

--- closure.go
+++ closure.go
@@ -4,44 +4,20 @@ import (
 	"fmt"
 )
 
-func foo(x int) []func() {
-	var fs []func()
-	var values = []int{1, 2, 3, 4, 5}
-	for _, val := range values {
-		fs = append(fs, func() {
-			fmt.Printf("foo value: \t%v\n", val+x)
-		})
-	}
+// 闭包=函数+引用环境
+// https://tiancaiamao.gitbooks.io/go-internals/content/zh/03.6.html
 
-	return fs
-}
-
-func Output() {
-	for i := 0; i < 10; i++ {
-		go func() {
-			fmt.Printf("%d ", i)
-		}()
-		// time.Sleep(1 * time.Second)
+func f(i int) func() int {
+	return func() int {
+		i++
+		return i
 	}
-	fmt.Printf("\nOutput() end\n\n")
-}
-
-func Output2() {
-	for i := 0; i < 10; i++ {
-		i := i
-		go func() {
-			fmt.Printf("%d ", i)
-		}()
-	}
-	fmt.Printf("\nOutput2() end\n\n")
 }
 
 func ClosureTest() {
-	var fs = foo(10)
-	for _, f := range fs {
-		f()
-	}
+	var c1 = f(0)
+	var c2 = f(0)
 
-	Output()
-	Output2()
+	fmt.Println(c1())
+	fmt.Println(c2())
 }
